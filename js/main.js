92ad4aa004eccb3a5e3f8dc15cc3e0315b81888b
Nuria Liaño <lianonuria@gmail.com>
2023-08-06 15:49:41 +0200

    change sections

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 addEventListener("load", function() {
     // Inicializar la primera palabra
     changeWordWithOpacityEffect();
-    // Obtenemos el elemento #text-explain y todas las tarjetas .services-card
-    const textExplain = document.getElementById('text-explain');
-    const servicesCards = document.querySelectorAll('.services-card');
+    // // Obtenemos el elemento #text-explain y todas las tarjetas .services-card
+    // const textExplain = document.getElementById('text-explain');
+    // const servicesCards = document.querySelectorAll('.services-card');
 
-    // Agregamos el evento 'mouseenter' a cada tarjeta
-    servicesCards.forEach(card => {
-        card.addEventListener('mouseenter', () => {
-            // Al hacer hover, actualizamos el contenido de #text-explain con el texto del atributo data-text
-            textExplain.textContent = card.getAttribute('data-text');
-        });
-    });
+    // // Agregamos el evento 'mouseenter' a cada tarjeta
+    // servicesCards.forEach(card => {
+    //     card.addEventListener('mouseenter', () => {
+    //         // Al hacer hover, actualizamos el contenido de #text-explain con el texto del atributo data-text
+    //         textExplain.textContent = card.getAttribute('data-text');
+    //     });
+    // });
+    infoServices();
 }, false);
 
 const wordsArray = ['Nuria Liaño', "profe", "sysadmin", "web developer", "de Cantabria", "metalera"];
@@ -50,4 +51,69 @@ function toggleMenu() {
     }
   }
 
-
+function infoServices() {
+      // Obtener todas las tarjetas
+      const servicesCards = document.querySelectorAll(".services-card");
+      // Agregar un evento "mouseover" a cada tarjeta
+      servicesCards.forEach((card) => {
+          card.addEventListener("mouseover", () => {
+              let cardId = card.id;
+              switch (cardId) {
+                case 'services-cp':
+                  document.getElementById("text-explain").textContent = "Clases particulares";
+                  document.getElementById("linktoweb").href = "www.skilly.es";
+                  document.getElementById("imglink").src = "media/img/7AC943_black_transparent.png";
+                  document.getElementById("ico-github").src = "media/img/icons/github.svg";
+                  document.getElementById("ico-gitlab").src = "media/img/icons/gitlab.svg";
+                  document.getElementById("repo-github").src = "https://gitlab.com/Nuria_Liano/skilly";
+                  document.getElementById("repo-gitlab").src = "https://github.com/NuriaLiano/skilly";
+                  break
+                case 'services-sy':
+                  document.getElementById("text-explain").textContent = "Administración de sistemas";
+                  document.getElementById("linktoweb").href = "";
+                  document.getElementById("imglink").src = "media/img/icons/soon.svg";
+                  document.getElementById("ico-github").src = "";
+                  document.getElementById("ico-gitlab").src = "";
+                  document.getElementById("repo-github").src = "";
+                  document.getElementById("repo-gitlab").src = "";
+                  break
+                case 'services-doc':
+                  document.getElementById("text-explain").textContent = "Documentación";
+                  document.getElementById("linktoweb").href = "www.nuridocu.es";
+                  document.getElementById("imglink").src = "media/img/logo_nuridocu_linea_blue.png";
+                  document.getElementById("ico-github").src = "media/img/icons/github.svg";
+                  document.getElementById("ico-gitlab").src = "media/img/icons/gitlab.svg";
+                  document.getElementById("repo-github").src = "https://gitlab.com/Nuria_Liano/nuridocu";
+                  document.getElementById("repo-gitlab").src = "https://github.com/NuriaLiano/nuridocu";
+                  break
+                case 'services-dw':
+                  document.getElementById("text-explain").textContent = "Desarrollo Web";
+                  document.getElementById("linktoweb").href = "";
+                  document.getElementById("imglink").src = "media/img/icons/soon.svg";
+                  document.getElementById("ico-github").src = "";
+                  document.getElementById("ico-gitlab").src = "";
+                  document.getElementById("repo-github").src = "";
+                  document.getElementById("repo-gitlab").src = "";
+                  break
+                case 'services-me':
+                  document.getElementById("text-explain").textContent = "Mantenimiento y reparación de equipos";
+                  document.getElementById("linktoweb").href = "";
+                  document.getElementById("imglink").src = "media/img/icons/soon.svg";
+                  document.getElementById("ico-github").src = "";
+                  document.getElementById("ico-gitlab").src = "";
+                  document.getElementById("repo-github").src = "";
+                  document.getElementById("repo-gitlab").src = "";
+                  break
+                case 'services-ve':
+                  document.getElementById("text-explain").textContent = "Venta de equipos informáticos";
+                  document.getElementById("linktoweb").href = "";
+                  document.getElementById("imglink").src = "media/img/icons/soon.svg";
+                  document.getElementById("ico-github").src = "";
+                  document.getElementById("ico-gitlab").src = "";
+                  document.getElementById("repo-github").src = "";
+                  document.getElementById("repo-gitlab").src = "";
+                  break
+              }
+          });
+      });
+}
