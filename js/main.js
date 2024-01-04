c6f6946abb7bdd10765a244c95f8896944da172b
Nuria Liaño <lianonuria@gmail.com>
2024-01-04 15:53:48 +0100

    fix contact-form

--- js/main.js
+++ js/main.js
@@ -130,3 +130,15 @@ function infoServices() {
       });
 }
 
+//captcha
+function onClick(e) {
+  e.preventDefault();
+  grecaptcha.enterprise.ready(async () => {
+    const token = await grecaptcha.enterprise.execute('6Lfozh0oAAAAAPHA3ELpG10aeq0OpxuSAbd4Pm9A', {action: 'LOGIN'});
+    // IMPORTANT: The 'token' that results from execute is an encrypted response sent by
+    // reCAPTCHA Enterprise to the end user's browser.
+    // This token must be validated by creating an assessment.
+    // See https://cloud.google.com/recaptcha-enterprise/docs/create-assessment
+  });
+}
+
